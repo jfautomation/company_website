75229e5f1812a4ffe6f19dd7955a8b16d58ab2e7
Tatyana <tatyanajfautomation@gmail.com>
2025-05-26 16:57:09 -0400

    added blog card

--- themes/my-custom-theme/partials/landing-page-blog.php
+++ themes/my-custom-theme/partials/landing-page-blog.php
@@ -1,8 +1,16 @@
 <section class="blog-section">
-    <div class="container">
-        <h2 class="mb-4">Latest from the Blog</h2>
 
-        <div class="row">
+    <div class="container">
+        <?php
+        $args = array(
+            'h5_text' => 'Blog',
+            'h2_text' => 'Recent posts'
+        );
+        get_template_part('partials/section-header', null, $args);
+        ?>
+
+
+        <div class="row mt-5">
             <?php
             $query = new WP_Query([
                 'post_type' => 'post',
@@ -11,23 +19,27 @@
 
             if ($query->have_posts()):
                 while ($query->have_posts()): $query->the_post(); ?>
-                    <div class="col-md-4 mb-4">
-                        <div class="card h-100">
-                            <?php if (has_post_thumbnail()): ?>
-                                <img src="<?php the_post_thumbnail_url('medium'); ?>" class="card-img-top" alt="<?php the_title(); ?>">
-                            <?php endif; ?>
-                            <div class="card-body">
-                                <h5 class="card-title"><?php the_title(); ?></h5>
-                                <p class="card-text"><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
-                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
-                            </div>
+            <div class="col-md-3 mb-4">
+                <div>
+                    <div class="">
+                        <?php if (has_post_thumbnail()): ?>
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>"
+                            class="card-img-top" alt="<?php echo esc_attr(get_the_title()); ?>">
+                        <?php endif; ?>
+
+
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <p class="card-text"><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
+                        <div>
+
                         </div>
                     </div>
-                <?php endwhile;
+                    <?php endwhile;
                 wp_reset_postdata();
             else: ?>
-                <p>No blog posts found.</p>
-            <?php endif; ?>
-        </div>
-    </div>
+                    <p>No blog posts found.</p>
+                    <?php endif; ?>
+                </div>
+            </div>
 </section>
